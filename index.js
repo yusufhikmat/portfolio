c05289aa9524a698e2ac41495671bea47fe6750d
Gelintech <hikmatyusuff2020@gmail.com>
2022-11-05 00:49:38 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const nav= document.querySelector('#nav-menu');
+const navLinks = document.querySelector('.nav-links');
+nav.addEventListener('click',showNav)
+function showNav(){
+    navLinks.classList.toggle('active')
+}
+
+/**contact form */
+
