7c82d6c70bf36918a97501faade7ce4ec3009434
Gelintech <hikmatyusuff2020@gmail.com>
2022-11-06 01:37:49 +0100

    commit msg

--- index.js
+++ index.js
@@ -5,5 +5,34 @@ function showNav(){
     navLinks.classList.toggle('active')
 }
 
+//navbar color change
+
+function changeNavbarBg(){
+    const scrollValue = window.scrollY;
+    const navMenu = document.getElementById('nav-menu');
+    
+    if(scrollValue < 600){
+        navMenu.classList.remove('change-navMenuBg')
+    
+    }else{
+        navMenu.classList.add('change-navMenuBg')
+    }
+}
+window.addEventListener('scroll', changeNavbarBg)
+/**end of change navmenu */
+
+//read more and readless of btnseefoodmenu
+const readMoreBtn = document.querySelector('.read-more');
+const aboutText= document.querySelector('.about-text');
+
+readMoreBtn.addEventListener('click',()=>{
+    aboutText.classList.toggle('show-text');
+    if(readMoreBtn.textContent === 'REAd MORE'){
+        readMoreBtn.textContent = 'READ LESS';
+    }else{
+        readMoreBtn.textContent = 'READ MORE';
+    }
+})
+
 /**contact form */
 
